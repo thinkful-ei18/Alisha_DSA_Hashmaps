675e1ddab8586ed7b88d062085e9b458b9c764ba
alishaevn <alieva.inc@gmail.com>
2018-04-09 09:53:50 -0700

    completed

--- Lor.js
+++ Lor.js
@@ -4,29 +4,67 @@ const HashMap = require('./HashMap');
 
 
 const Lor = () => {
-
   const lor = new HashMap();
 
   const CHARACTERS = [
-    { Hobbit: 'Bilbo' },
-    { Hobbit: 'Frodo' },
-    { Wizard: 'Gandolf' },
-    { Human: 'Aragon' },
-    { Elf: 'Legolas' },
-    { Maiar: 'The Necromancer' },
-    { Maiar: 'Sauron' },
-    { RingBearer: 'Gollum' },
-    { LadyOfLight: 'Galadriel' },
-    { HalfElven: 'Arwen' },
-    { Ent: 'Treebeard' }
+    { Key: 'Hobbit', Value: 'Bilbo' }, 
+    { Key: 'Hobbit', Value: 'Frodo' }, 
+    { Key: 'Wizard', Value: 'Gandolf' }, 
+    { Key: 'Human', Value: 'Aragon' }, 
+    { Key: 'Elf', Value: 'Legolas' },
+    { Key: 'Maiar', Value: 'The Necromancer' },
+    { Key: 'Maiar', Value: 'Sauron' },
+    { Key: 'RingBearer', Value: 'Gollum' },
+    { Key: 'LadyOfLight', Value: 'Galadriel' },
+    { Key: 'HalfElven', Value: 'Arwen' },
+    { Key: 'Ent', Value: 'Treebeard' },
   ];
 
-  // CHARACTERS.map(characterObj => lor.set(Object.keys(characterObj), Object.values(characterObj)));
+  // inserting 11 objects, with an initial capacity of 8 slots
+  CHARACTERS.map(character => lor.set((character.Key), (character.Value)));
 
-  CHARACTERS.map(characterObj => console.log(Object.keys(characterObj), Object.values(characterObj)));
+  console.log('lor', lor);
 
+  /*
+  - Each '' represents an empty index
+  - the capacity has been tripled to 24 per HashMap.SIZE.RATIO
+  - indexes are chosen based on the KEY, when you add a new value that has the same key, the initial value of that key is overridden
 
+  lor = {
+    _slots: [ 
+      '',
+      '',
+      { key: 'HalfElven', value: 'Arwen', deleted: false },
+      '',
+      { key: 'LadyOfLight', value: 'Galadriel', deleted: false },
+      '',
+      { key: 'Wizard', value: 'Gandolf', deleted: false },
+      { key: 'RingBearer', value: 'Gollum', deleted: false },
+      '',
+      '',
+      '',
+      '',
+      { key: 'Elf', value: 'Legolas', deleted: false },
+      { key: 'Hobbit', value: 'Frodo', deleted: false },
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      { key: 'Ent', value: 'Treebeard', deleted: false },
+      '',
+      { key: 'Human', value: 'Aragon', deleted: false },
+      { key: 'Maiar', value: 'Sauron', deleted: false } 
+    ],
+    length: 9,
+    _capacity: 24
+  }
+  */
 
+  return lor.get('Maiar');
+  // this returns 'Sauron' because 'The Necromancer' (same key) was overwritten 
+  
 };
 
 console.log(Lor());
